35179a2e54ec570e167a11fe0bc579a8dfaeaa95
Yunhyok Choi <yunhyok@hotmail.com>
2024-09-23 11:33:49 +0900

    Add project files.

---
+++ Power Supply and VNA Controller/Form1.Designer.vb
@@ -0,0 +1,38 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        SuspendLayout()
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(144F, 144F)
+        AutoScaleMode = AutoScaleMode.Dpi
+        ClientSize = New Size(1178, 844)
+        Font = New Font("Malgun Gothic", 11F, FontStyle.Regular, GraphicsUnit.Point, CByte(129))
+        Name = "Form1"
+        Text = "Instrument Automation"
+        ResumeLayout(False)
+    End Sub
+
+End Class
